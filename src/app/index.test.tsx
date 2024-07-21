f2ae66280de3952c164554ace0e732448cbcdca9
Gelson Rodrigues <GelsonMarianoRodrigues@gmail.com>
2024-07-21 13:47:16 -0300

    test: add main test scenarios

--- src/app/index.test.tsx
+++ src/app/index.test.tsx
@@ -7,4 +7,22 @@ describe('App component', () => {
     const linkElement = screen.getByText(/SearchLion/i);
     expect(linkElement).toBeInTheDocument();
   });
+
+  test.todo('renders application form, search field and button named "Search"');
+
+  test.todo('triggers search on button click and displays results');
+
+  test.todo(
+    'triggers search on enter press while focused on input field and displays results'
+  );
+
+  test.todo('renders loading state until search results are available');
+
+  test.todo('renders title and description of each search result');
+
+  test.todo('click on a search result opens it in a new tab');
+
+  test.todo('renders user-friendly content type of each search result');
+
+  test.todo('renders empty state when no results are found');
 });
